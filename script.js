b57c6e73eff2856632a2b77fc5e90e8ad026cbab
ihsan <ihsanberkozcan@gmail.com>
2021-02-20 23:47:15 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const daysE1 = document.querySelector("#days");
+const hoursE1 = document.querySelector("#hours");
+const minutsE1 = document.querySelector("#minutes");
+const secondsE1 = document.querySelector("#seconds");
+
+
+const newYears = "1 Jan 2022"
+
+function countDown() {
+    const newYearsDate = new Date(newYears)
+    const currentDate = new Date()
+    
+    const totalSecond = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSecond / (3600 * 24));
+    const hours = Math.floor(totalSecond / 3600) % 24;
+    const minuts = Math.floor(totalSecond / 60) % 60;
+    const seconds = Math.floor(totalSecond) % 60;
+
+    daysE1.innerHTML = days ;
+    hoursE1.innerHTML = formatTime(hours);
+    minutsE1.innerHTML = formatTime(minuts);
+    secondsE1.innerHTML = formatTime(seconds);
+
+}
+
+function formatTime(time){
+    return time<10 ? (`0${time}`):time;
+}
+
+countDown();
+setInterval(countDown, 1000);
